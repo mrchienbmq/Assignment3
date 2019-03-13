0562872bd7c5df91fe9c6423a0edbe4ac618e89e
mrchienbmq <31257719+mrchienbmq@users.noreply.github.com>
2019-03-13 23:12:47 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+package lab1_session3;
+
+public class Main {
+    public static void main(String[] args){
+        SoNguyenTo soNguyenTo = new SoNguyenTo();
+        soNguyenTo.isSoNguyenTo();
+    }
+}
