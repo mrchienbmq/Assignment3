c61d7cef74a127f290aa9e1d380550f03eb062e8
mrchienbmq <31257719+mrchienbmq@users.noreply.github.com>
2019-03-13 23:13:09 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package lab1_session3;
-
-public class Main {
-    public static void main(String[] args){
-        SoNguyenTo soNguyenTo = new SoNguyenTo();
-        soNguyenTo.isSoNguyenTo();
-    }
-}
